f6850a051125c9503a8d1bdbe7b64256f444c300
Marius <marius_kata94@yahoo.com>
2019-01-04 21:09:44 +0200

    Update Logo and Sidebar Menu

--- resources/views/layouts/adminLayout/admin_sidebar.blade.php
+++ resources/views/layouts/adminLayout/admin_sidebar.blade.php
@@ -1,17 +1,18 @@
+<?php $url = url()->current(); ?>
 <!--sidebar-menu-->
 <div id="sidebar"><a href="#" class="visible-phone"><i class="icon icon-home"></i> Dashboard</a>
   <ul>
-    <li class="active"><a href="{{ url('/admin/dashboard') }}"><i class="icon icon-home"></i> <span>Dashboard</span></a> </li>
+    <li <?php if (preg_match("/dashboard/i", $url)){ ?> class="active" <?php } ?>> <a href="{{ url('/admin/dashboard') }}"><i class="icon icon-home"></i> <span>Dashboard</span></a> </li>
     <li class="submenu"> <a href="#"><i class="icon icon-th-list"></i> <span>Categories</span> <span class="label label-important">2</span></a>
-      <ul>
-        <li><a href="{{ url('/admin/add-category') }}">Add Category</a></li>
-        <li><a href="{{ url('/admin/view-categories') }}">View Categories</a></li>
+      <ul <?php if (preg_match("/categor/i", $url)){ ?> style="display: block;" <?php } ?>>
+        <li <?php if (preg_match("/add-category/i", $url)){ ?> class="active" <?php } ?>><a href="{{ url('/admin/add-category') }}">Add Category</a></li>
+        <li <?php if (preg_match("/view-categories/i", $url)){ ?> class="active" <?php } ?>><a href="{{ url('/admin/view-categories') }}">View Categories</a></li>
       </ul>
     </li>
     <li class="submenu"> <a href="#"><i class="icon icon-th-list"></i> <span>Products</span> <span class="label label-important">2</span></a>
-      <ul>
-        <li><a href="{{ url('/admin/add-product') }}">Add Product</a></li>
-        <li><a href="{{ url('/admin/view-products') }}">View Products</a></li>
+      <ul <?php if (preg_match("/product/i", $url)){ ?> style="display: block;" <?php } ?>>
+        <li <?php if (preg_match("/add-product/i", $url)){ ?> class="active" <?php } ?>><a href="{{ url('/admin/add-product') }}">Add Product</a></li>
+        <li <?php if (preg_match("/view-product/i", $url)){ ?> class="active" <?php } ?>><a href="{{ url('/admin/view-products') }}">View Products</a></li>
       </ul>
     </li>
   </ul>
